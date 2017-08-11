256299d81d168e9a442e8944496a49bb5eb118aa
Gurz Singh <Gurz Singh@172.20.146.165>
2017-08-11 16:42:46 +0100

    Calculator added

---
+++ GitProject/src/gurzpack/Calculator.java
@@ -0,0 +1,14 @@
+package gurzpack;
+
+public class Calculator {
+	public static int add(int i, int j)
+	{
+		return i+j;
+	}
+	public static void main(String[] args) {
+		
+		int x=add(1232, 123);
+		System.out.println(x);
+	}
+
+}
